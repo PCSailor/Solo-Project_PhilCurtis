80544d368149e2c3ef985c203118de2c5854466d
Phil Curtis <Phil@PhilCurtis.io>
2017-03-21 11:28:57 -0500

    source files verified AND app.js added

---
+++ server/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var routesJS = require('./routes/routes.js');
+// NOTE: Middleware
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+// OR:
+app.use(bodyParser.json()); // NOTE: use with factories
+// NOTE: app.use(bodyParser...) creates req.body
+app.use('/arrayList',routesJS);
+// TODO: arrayList is intertwined in html pages
+
+app.listen (5500, function() {
+  console.log("app.js loaded & server listening to port 5500");
+};
