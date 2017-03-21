cb77b7b70e6e6a119c434cebde3b05b938121764
Phil Curtis <Phil@PhilCurtis.io>
2017-03-20 21:59:46 -0500

    everything-working-in-index-html_bringing in AngJS-and-clientJS

---
+++ server/scripts/client.js
@@ -0,0 +1,5 @@
+console.log('JS script loaded');
+// var soloProjectApp = angular.module('soloProjectAngularApp', ['ngRoute']);
+// NOTE: soloProjectAngularApp on clientJS, Index.html
+// NOTE: ngRoute matched to indexHTML/ng-view AND is a dependency of
+// QUESTION: How to have ng-route point to different ng-views with different information?
